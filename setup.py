cb7091659424471068446b0c7d5b4e1365153626
André Panisson <panisson@gmail.com>
2012-07-20 17:36:31 +0200

    setup script update

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 
 setup(name="pymobility",
-      version="0.1",
-      description="Mobility models for python",
+      version="0.1.0",
+      description="Mobility models for Python",
       license="GPL",
       author="André Panisson",
       author_email="panisson@gmail.com",
@@ -14,4 +15,15 @@ setup(name="pymobility",
       package_dir = {'': 'src'},
       include_package_data=True,
       install_requires=['numpy', 'matplotlib',],
+      classifiers=[
+                   'License :: OSI-Approved Open Source :: GNU General Public License version 2.0 (GPLv2)',
+                   'Programming Language :: Python',
+                   'Operating System :: OS Independent',
+                   'Environment :: Console',
+                   'User Interface :: Textual :: Command-line'
+                   'Topic :: Scientific/Engineering :: Simulations',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Science/Research',
+                   ],
       )
